f79198acfb243bf8323612ec5b99fbe0a673ae01
Conor Haselden <conor_th@hotmail.co.uk>
2013-11-14 17:16:39 +0000

    Created fourierultimate.m

--- 1d/lorentzian.m
+++ 1d/lorentzian.m
@@ -1,7 +1,7 @@
-function [y] = lorentzian(x, s, l, x0)
+function [y] = lorentzian(x, s, l)
 g = ones(1,length(x))*s/2;
 y = zeros(1, length(x));
 for i = 1:50
-    % x0 = -l/2 + l*rand(1);
-    y = y - (1/pi)*g./((x-x0(i)).^2 + g.^2);
+    x0 = -l/2 + l*rand(1);
+    y = y - (1/pi)*g./((x-x0).^2 + g.^2);
 end
---
+++ fourierultimate.m
@@ -0,0 +1,46 @@
+% Script to plot the Fourier Transform of a given function
+
+% Define the sampling frequency
+SF = 5;
+% Create a vector of 10^5 points between -10 and 10
+t = -250:1/SF:250;
+% Input given function
+x = input('input function: ');
+% Minimum length of FFT multiplied by 20
+n = (2^5)*(2^nextpow2(length(t))); % Length of FFT
+% Apply Fast Fourier Transform
+X = fft(x,n); 
+% FFT is symmetric, throw away second half
+X = X(1:n/2); 
+% Ignore imaginary values of X
+Y = abs(X);
+% Normalise the frequency scale
+f = (0:n/2-1)*SF/n;
+
+satisfied = false;
+
+while (~satisfied)
+
+    cutoff1 = input('where do you want cutoff1? (index, 200 is good): ');
+    cutoff2 = input('where do you want cutoff2? (value): ');
+
+    % THESE LINES FOR CUTTING OF ARTEFACTS
+    for i = 1:length(f)
+        if(f(i) > cutoff2)
+            f = f(cutoff1:i); 
+            Y = Y(cutoff1:i);
+            Y2 = log(Y);
+            break;
+        end
+    end
+    
+    semilogy(f,Y);
+    hold all;
+    xlabel('Frequency (Hz)','interpreter','Latex','FontSize',15); 
+    ylabel('Power','interpreter','Latex','FontSize',15);
+    legend('Fourier transform','Smoothed Fourier transform');
+    % Limit the x axis (Is there a better way to scale it?)
+    xlim([0,0.3]);
+
+    satisfied = input('Satisfied? (true, false): ');
+end
