35473ecfc384d35ec93eb2fcb4302d1d5c0b080d
Tim Fogarty <timothy.fogo@gmail.com>
2013-11-14 17:13:52 +0000

    Renames fig and makes 2 more Lorentzian functions

---
+++ 1d/lorentzianSum2.m
@@ -0,0 +1,7 @@
+function [y] = lorentzianSum2(x, s, l)
+g = ones(1,length(x))*s/2;
+y = zeros(1, length(x));
+for i = 1:50
+    x0 = -l/2 + l*rand(1);
+    y = y - (1/pi)*g./((x-x0).^2 + g.^2);
+end
